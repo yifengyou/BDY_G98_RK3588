03144d29d7314470f103270e3ec46da2ba5e6cc0
yifengyou <842056007@qq.com>
2026-09-23 17:06:30 +0800

    update linux-stable workflow
    Signed-off-by: yifengyou <842056007@qq.com>

--- kdevbuild/linux_stable_image.sh
+++ kdevbuild/linux_stable_image.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 LOG_FILE="${WORKDIR}/build.log"
 exec > >(tee -a "$LOG_FILE") 2>&1
-export BUILD_TAG="BDY_G98_stable_${set_rootfs}"
+export BUILD_TAG="BDY_G98_linux-stable_${set_rootfs}"
 
 #==========================================================================#
 #                        init build env                                    #
